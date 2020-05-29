27fe90fedd60f28d86a7e97f0a4e75ef1d59cd3a
garuav <gbgaurav461@gmail.com>
2020-05-29 22:41:01 +0530

    added url and imported the postman-request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,17 @@
-console.log('Starting');
+// console.log('Starting');
 
-setTimeout(() => {
-    console.log('2 Second Timer');
-},2000);
-
-
-setTimeout(() => {
-    console.log('0 Second Timer');
-},0);
+// setTimeout(() => {
+//     console.log('2 Second Timer');
+// },2000);
 
 
-console.log('Stopping');
+// setTimeout(() => {
+//     console.log('0 Second Timer');
+// },0);
+
+
+// console.log('Stopping');
+
+var request = require('postman-request');
+
+const url = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
