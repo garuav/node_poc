a0c3e2623f0a89eced9af76cb6eef413f1ada009
garuav <gbgaurav461@gmail.com>
2020-05-30 08:03:17 +0530

    getting json data

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,6 +12,11 @@
 
 // console.log('Stopping');
 
-var request = require('postman-request');
+const request = require('postman-request');
 
 const url = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
+
+request({url: url, json: true }, (error, response) => {
+    const data = response.body.current;
+    console.log(`It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
+})
