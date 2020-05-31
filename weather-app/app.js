2e07bfa9b96afc765a951ae44fcaaec0d78cff5f
garuav <gbgaurav461@gmail.com>
2020-05-31 16:53:29 +0530

    implemented callback for geo code

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,37 +14,45 @@
 
 const request = require('postman-request');
 
-const url_weather_stack = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
+// const url_weather_stack = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
 
-request({url: url_weather_stack, json: true }, (error, response) => {
-    if(error) {
-        console.log('Unable to connect with weatherstack service !');
-    } else if(response.body.error) {
-        console.log('Unable to find Location !');
-    }
-     else {
-        const data = response.body.current;
-        console.log(`${data.weather_descriptions[0]} It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
-    }
+// request({url: url_weather_stack, json: true }, (error, response) => {
+//     if(error) {
+//         console.log('Unable to connect with weatherstack service !');
+//     } else if(response.body.error) {
+//         console.log('Unable to find Location !');
+//     }
+//      else {
+//         const data = response.body.current;
+//         console.log(`${data.weather_descriptions[0]} It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
+//     }
     
-});
+// });
 
 
-// mapbox request
-const url_mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/BTM.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality';
 
-request({url: url_mapbox, json: true}, (error,response) => {
-    console.log('response = ',response);
+const getGeoCode = (address, callback) => {
+const url_mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality`;
+request({url: url_mapbox, json: true}, (error, response ) => {
+        if(error) {
+            callback('Unable to connect with location services !', undefined);
+        } else if(!response.body.features || response.body.features.length  === 0) {
+            callback('Unable to fetch location. Try another location ', undefined);
+        } else {
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location_name: response.body.features[0].place_name
+            });
+        }
+    })
+};
+
+getGeoCode('BTM Stage 1', (error, response ) => {
     if(error) {
-        console.log('Unable to connect with mapbox service !');
-    } else if(!response.body.features || response.body.error) {
-        console.log('Unable to fetch Location !')
+        console.log('error = ', error);
+    } else {
+        console.log('response = ',response);
     }
-     else {
-    const data = response.body;
-    const coordinates = data.features[0].geometry.coordinates[1] + ','+ data.features[0].geometry.coordinates[0];
-    console.log('data = ',data);
-    console.log('coordinates = ',coordinates);
-    }
+})
 
-});
