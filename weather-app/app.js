8b9e07745f345815ad25f884e2b14206070de6b3
garuav <gbgaurav461@gmail.com>
2020-05-31 17:01:43 +0530

    created a repo for geo code function

--- weather-app/app.js
+++ weather-app/app.js
@@ -13,7 +13,7 @@
 // console.log('Stopping');
 
 const request = require('postman-request');
-
+const geoCode = require('./utils/geocode');
 // const url_weather_stack = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
 
 // request({url: url_weather_stack, json: true }, (error, response) => {
@@ -31,24 +31,9 @@ const request = require('postman-request');
 
 
 
-const getGeoCode = (address, callback) => {
-const url_mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality`;
-request({url: url_mapbox, json: true}, (error, response ) => {
-        if(error) {
-            callback('Unable to connect with location services !', undefined);
-        } else if(!response.body.features || response.body.features.length  === 0) {
-            callback('Unable to fetch location. Try another location ', undefined);
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].center[0],
-                longitude: response.body.features[0].center[1],
-                location_name: response.body.features[0].place_name
-            });
-        }
-    })
-};
 
-getGeoCode('BTM Stage 1', (error, response ) => {
+
+geoCode.getGeoCode('BTM Stage 1', (error, response ) => {
     if(error) {
         console.log('error = ', error);
     } else {
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('postman-request');
+const getGeoCode = (address, callback) => {
+    const url_mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality`;
+    request({url: url_mapbox, json: true}, (error, response ) => {
+            if(error) {
+                callback('Unable to connect with location services !', undefined);
+            } else if(!response.body.features || response.body.features.length  === 0) {
+                callback('Unable to fetch location. Try another location ', undefined);
+            } else {
+                callback(undefined, {
+                    latitude: response.body.features[0].center[0],
+                    longitude: response.body.features[0].center[1],
+                    location_name: response.body.features[0].place_name
+                });
+            }
+        })
+    };
+
+    module.exports = {
+        getGeoCode: getGeoCode
+    }
