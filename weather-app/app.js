d0395b68642c5ba0c894114eeacc6659e4559c91
garuav <gbgaurav461@gmail.com>
2020-05-31 15:38:33 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,9 +14,37 @@
 
 const request = require('postman-request');
 
-const url = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
+const url_weather_stack = "http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=12.9241,%2077.6092";
 
-request({url: url, json: true }, (error, response) => {
-    const data = response.body.current;
-    console.log(`${data.weather_descriptions[0]} It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
-})
+request({url: url_weather_stack, json: true }, (error, response) => {
+    if(error) {
+        console.log('Unable to connect with weatherstack service !');
+    } else if(response.body.error) {
+        console.log('Unable to find Location !');
+    }
+     else {
+        const data = response.body.current;
+        console.log(`${data.weather_descriptions[0]} It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`);
+    }
+    
+});
+
+
+// mapbox request
+const url_mapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/BTM.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality';
+
+request({url: url_mapbox, json: true}, (error,response) => {
+    console.log('response = ',response);
+    if(error) {
+        console.log('Unable to connect with mapbox service !');
+    } else if(!response.body.features || response.body.error) {
+        console.log('Unable to fetch Location !')
+    }
+     else {
+    const data = response.body;
+    const coordinates = data.features[0].geometry.coordinates[1] + ','+ data.features[0].geometry.coordinates[0];
+    console.log('data = ',data);
+    console.log('coordinates = ',coordinates);
+    }
+
+});
