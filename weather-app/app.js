fae7a46f82b7f76c3a454d16a97b5d49176da0f3
garuav <gbgaurav461@gmail.com>
2020-06-02 23:02:45 +0530

    added code for taking location dynamically from command line argument

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,27 @@
-const request = require('postman-request');
 const geoCode = require('./utils/geocode');
 const forecaste = require('./utils/forecaste');
 
-
-geoCode.getGeoCode('BTM Stage 1', (error, response ) => {
-    if(error) {
-       return  console.log('error = ', error);
-    }
-        // console.log('response = ',response);
-        // console.log(`Location = ${response.location_name}. With latitude ${response.latitude} and longitude ${response.longitude}`);
-
-        forecaste.getForecaste(response.latitude, response.longitude , (forecasteError, forecasteResponse ) => {
-            if(error) {
-               return  console.log('forecasteError = ', forecasteError);
-            } 
-                // console.log('forecasteResponse = ', forecasteResponse);
-                console.log(`${response.location_name} Weather is ${forecasteResponse.weather_descriptions[0]}. Temparature is ${forecasteResponse.temperature} degrees it feels like ${forecasteResponse.feelslike} degrees out.`);
-
-            
-        })
+if(process.argv[2]) {
+    geoCode.getGeoCode(process.argv[2], (error, response ) => {
+        if(error) {
+           return  console.log('error = ', error);
+        }
+            // console.log('response = ',response);
+            // console.log(`Location = ${response.location_name}. With latitude ${response.latitude} and longitude ${response.longitude}`);
     
-})
+            forecaste.getForecaste(response.latitude, response.longitude , (forecasteError, forecasteResponse ) => {
+                if(error) {
+                   return  console.log('forecasteError = ', forecasteError);
+                } 
+                    // console.log('forecasteResponse = ', forecasteResponse);
+                    console.log(`${response.location_name} Weather is ${forecasteResponse.weather_descriptions[0]}. Temparature is ${forecasteResponse.temperature} degrees it feels like ${forecasteResponse.feelslike} degrees out.`);
+    
+                
+            })
+        
+    })
+} else {
+    console.log('Please provide the location from the command line argument.')
+}
+
 
