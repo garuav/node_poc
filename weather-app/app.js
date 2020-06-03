eecef36c6ab55b8a00c90bf0b63c999f7b1b8232
garuav <gbgaurav461@gmail.com>
2020-06-03 08:37:55 +0530

    destructing used in function

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,19 +2,19 @@ const geoCode = require('./utils/geocode');
 const forecaste = require('./utils/forecaste');
 
 if(process.argv[2]) {
-    geoCode.getGeoCode(process.argv[2], (error, response ) => {
+    geoCode.getGeoCode(process.argv[2], (error, {latitude, longitude, location_name} = {} ) => {
         if(error) {
            return  console.log('error = ', error);
         }
             // console.log('response = ',response);
             // console.log(`Location = ${response.location_name}. With latitude ${response.latitude} and longitude ${response.longitude}`);
     
-            forecaste.getForecaste(response.latitude, response.longitude , (forecasteError, forecasteResponse ) => {
+            forecaste.getForecaste(latitude, longitude , (forecasteError, {weather_descriptions, temperature, feelslike} ={} ) => {
                 if(error) {
                    return  console.log('forecasteError = ', forecasteError);
                 } 
                     // console.log('forecasteResponse = ', forecasteResponse);
-                    console.log(`${response.location_name} Weather is ${forecasteResponse.weather_descriptions[0]}. Temparature is ${forecasteResponse.temperature} degrees it feels like ${forecasteResponse.feelslike} degrees out.`);
+                    console.log(`${location_name} Weather is ${weather_descriptions[0]}. Temparature is ${temperature} degrees it feels like ${feelslike} degrees out.`);
     
                 
             })
