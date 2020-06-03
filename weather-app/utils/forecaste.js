926055ce6704800bbf876285497923863147754a
garuav <gbgaurav461@gmail.com>
2020-06-03 08:43:56 +0530

    destructing and short hand used in geocode  file function

--- weather-app/utils/forecaste.js
+++ weather-app/utils/forecaste.js
@@ -3,7 +3,7 @@ const request = require('postman-request');
 
 const getForecaste = (latitude, longitude, callback) => {
 const url = `http://api.weatherstack.com/current?access_key=ec382bea358926841dadb2b2868b2974&query=${latitude},${longitude}`;
-    request({url, json: true}, (error, {body}) => {
+    request({url, json: true}, (error, {body} = {}) => {
         if(error) {
             callback('Unable to connect with weather server !', undefined);
         } else if(body.error) {
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,16 +1,16 @@
 const request = require('postman-request');
 const getGeoCode = (address, callback) => {
-    const url_mapbox = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality`;
-    request({url: url_mapbox, json: true}, (error, response ) => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURI(address)}.json?access_token=pk.eyJ1IjoiZ2JnYXVyYXYiLCJhIjoiY2thdDE3ZzBsMDc5ZDMxcGhybHhmOWFwNSJ9.oxoqYs7miIRCxc3FSlYQJg&limit=1&type=locality`;
+    request({url, json: true}, (error, {body} = {} ) => {
             if(error) {
                 callback('Unable to connect with location services !', undefined);
-            } else if(!response.body.features || response.body.features.length  === 0) {
+            } else if(!body.features || body.features.length  === 0) {
                 callback('Unable to fetch location. Try another location ', undefined);
             } else {
                 callback(undefined, {
-                    latitude: response.body.features[0].center[1],
-                    longitude: response.body.features[0].center[0],
-                    location_name: response.body.features[0].place_name
+                    latitude: body.features[0].center[1],
+                    longitude: body.features[0].center[0],
+                    location_name: body.features[0].place_name
                 });
             }
         })
